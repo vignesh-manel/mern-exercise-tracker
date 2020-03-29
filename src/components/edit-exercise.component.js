5c54fafd6ae865497838d9eba7c77b0863b43495
vmanel <vnayak83@gmail.com>
2020-03-29 15:48:37 +0000

    Setup Navbar and component stub

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditExercise extends Component {
+  render() {
+    return (
+      <div>
+	<p>You are on edit exercise component</p>
+      </div>
+    );
+  }
+}
