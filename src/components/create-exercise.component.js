afc6e48d60ae3f0b31632750f575db3a173892c2
vmanel <vnayak83@gmail.com>
2020-04-05 12:21:28 +0000

    Connected front-end to backend using axios

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -24,11 +25,15 @@ export default class CreateExercise extends Component {
   }
 
   componentDidMount() {
-    this.setState({
-	users: ['test user'],
-	username: 'test user',
-	
-    })
+    axios.get('http://localhost:5000/users')
+	.then(res => {
+	  if (res.data.length > 0) {
+	    this.setState({
+		users: res.data.map(user => user.username),
+		username: res.data[0].username
+	    });
+	  }
+	})
 
   }
 
@@ -66,6 +71,10 @@ export default class CreateExercise extends Component {
 	date: this.state.date
     }
 
+    console.log(exercise);
+
+    axios.post('http://localhost:5000/exercises/add',exercise)
+	.then(res => console.log(res.data));
 
     window.location = '/';
   }
